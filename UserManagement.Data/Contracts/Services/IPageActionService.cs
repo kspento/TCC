0c6a05fe2dd68bf7a4a6c80d6d2135359ff0dc21
Faria <Faria@DESKTOP-60KI7T9>
2023-10-12 18:29:28 -0300

    Contract fix

---
+++ UserManagement.Data/Contracts/Services/IPageActionService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UserManagement.Data.Dto.PageAction;
+using UserManagement.Domain.Model.PageAction;
+using UserManagement.MediatR.Queries;
+namespace UserManagement.Domain.Contracts.Services
+{
+    public interface IPageActionService
+    {
+        Task<PageActionDto> AddPageAction(AddPageActionModel request);
+        Task DeletePageAction(DeletePageActionModel request);
+        Task<List<PageActionDto>> GetAllPageActions();
+        Task<PageActionDto> GetPageAction(GetPageActionModel request);
+    }
+}
